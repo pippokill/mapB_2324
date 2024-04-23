80c0668a3ff5ba9d9b935153df9a39b427e43ac8
pippokill <pierpaolo.basile@gmail.com>
2024-04-23 13:24:37 +0200

    - RTTI

---
+++ lab/src/main/java/di/uniba/map/b/lab/rtti/SweetShop.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (C) 2020 pierpaolo
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package di.uniba.map.b.lab.rtti;
+
+/**
+ *
+ * @author pierpaolo
+ */
+class Candy {
+
+    static {  // questa è una clausola statica
+        System.out.println("Loading Candy");
+    }
+}
+
+class Cookie {
+
+    static {  // questa è una clausola statica
+        System.out.println("Loading Cookie");
+    }
+}
+
+/**
+ *
+ * @author pierpaolo
+ */
+public class SweetShop {
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("inside main");
+        new Candy();
+        new Candy();
+        try {
+            //Class<Candy> candyClass = Candy.class;
+            Class<?> candyClass = Class.forName("di.uniba.map.b.lab.rtti.Candy");
+            System.out.println(candyClass.getName());
+            //Candy.class.newInstance();
+            System.out.println("After creating Candy");
+            Class.forName("Gum");
+        } catch (ClassNotFoundException e) {
+            System.err.println(e);
+        //} catch (InstantiationException | IllegalAccessException ex) {
+            //System.err.println(e);
+        }
+        System.out.println("After Class.forName(\"Gum\")");
+        new Cookie();
+        new Cookie();
+        System.out.println("After creating Cookie");
+    }
+}
